7dc60f3db06565d68bc0bf6c129ad55c41659bec
Huan Nguyen <famish99@gmail.com>
2012-07-08 11:45:56 -0500

    Added class finder methods for DeckUser

--- game/models/session.py
+++ game/models/session.py
@@ -11,10 +11,38 @@ class DeckUser(caching.base.CachingMixin, models.Model):
     to either Session or Player
     """
 
+    classname = models.CharField(max_length=64, editable=False, null=True)
+
     objects = caching.base.CachingManager()
 
     def __init__(self, *args, **kwargs):
         super(DeckUser, self).__init__(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        """
+        Overwrite the save function to save the class name for referencing
+        purposes.
+        """
+        if not self.classname:
+            cur_class = self.__class__
+            class_str = cur_class.__name__
+            while cur_class.__name__ != "DeckUser":
+                cur_class = cur_class.__base__().__class__
+                class_str = "%s.%s" % (cur_class.__name__, class_str)
+            self.classname = class_str
+        super(DeckUser, self). save(*args, **kwargs)
+
+    def get_class(self):
+        """
+        Allow a user to query the main DeckUser table and be able to find
+        the associate subclass
+        """
+        if self.classname == self.__class__.__name__:
+            return self
+        ptr = self
+        for sub_class in self.classname.lower().split('.')[1:]:
+            ptr = ptr.__getattribute__(sub_class)
+        return ptr
 
     class Meta:
         """ Metadata class for Player """
