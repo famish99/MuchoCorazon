e30e15c6b1472ecf1a566dcfe7b41964f5287eb8
Huan Nguyen <famish99@gmail.com>
2012-07-08 21:08:33 -0500

    Added class trace test

--- game/tests.py
+++ game/tests.py
@@ -1,9 +1,7 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Unit testing for the app
 """
+
 
 from game.models.user import User
 from game.models.player import Player
@@ -12,13 +10,30 @@ from django.test import TestCase
 
 
 class DeckUserTestCase(TestCase):
+    """
+    Test associated DeckUser features
+    """
+
     def setUp(self):
         self.user = User.objects.create(name="Sugisaki Ken")
         self.session = Session.objects.create(turn=0, phase=0)
         self.player = Player.objects.create(
                 session=self.session, user=self.user)
 
-    def test_classname(self):
-        """ Check classname saving """
+    def test_class_name(self):
+        """
+        Check classname saving
+        """
         self.assertEqual(self.player.classname, "DeckUser.Player")
         self.assertEqual(self.session.classname, "DeckUser.Session")
+
+    def test_class_trace(self):
+        """
+        Check classname tracing
+        """
+        self.assertEqual(
+                DeckUser.objects.get(id=self.player.id).get_class(),
+                self.player)
+        self.assertEqual(
+                DeckUser.objects.get(id=self.session.id).get_class(),
+                self.session)
