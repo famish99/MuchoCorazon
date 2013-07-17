b9357087d3772dd77d0998c97286a9de9250c54e
Huan Nguyen <famish99@gmail.com>
2013-07-17 11:45:36 -0500

    Adding player secret field

--- game/models/player.py
+++ game/models/player.py
@@ -5,6 +5,7 @@ from game.models.user import UserProfile
 from game.models.session import DeckUser, Session
 from django.db import models
 import caching.base
+import uuid
 
 
 class Player(DeckUser):
@@ -15,8 +16,12 @@ class Player(DeckUser):
     user = models.ForeignKey(UserProfile, related_name="players")
     session = models.ForeignKey(Session, related_name="players")
 
+    # Used to protect json requests being made by other players
+    player_key = models.CharField(max_length=32, editable=False)
+
     def __init__(self, *args, **kwargs):
         super(Player, self).__init__(*args, **kwargs)
+        player_key = uuid.uuid4().hex
 
     class Meta:
         """ Metadata class for Player """
